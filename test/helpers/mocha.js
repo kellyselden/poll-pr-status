434f84b5c36b907693c86d6465c07c174127c3c4
Kelly Selden <kellyselden@gmail.com>
2022-01-01 16:13:15 -0800

    add setUpSinon

--- test/helpers/mocha.js
+++ test/helpers/mocha.js
@@ -1,3 +1,20 @@
 'use strict';
 
 require('mocha-helpers')(module);
+
+function setUpSinon() {
+  // eslint-disable-next-line prefer-let/prefer-let
+  const sinon = require('sinon');
+
+  global.before(function() {
+    this.sinon = sinon;
+  });
+
+  global.afterEach(function() {
+    sinon.restore();
+  });
+}
+
+Object.assign(module.exports, {
+  setUpSinon
+});
