af198fd6ec0029f41f1bf1db055bff6ce86fdd1e
Kelly Selden <kellyselden@gmail.com>
2022-01-01 16:15:31 -0800

    add tests

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const { promisify } = require('util');
-const request = promisify(require('request'));
 const { URL } = require('url');
 const ci = require('ci-info');
 const execa = require('execa');
@@ -12,6 +11,9 @@ function getTime() {
 }
 
 async function getStatus({
+  cwd = process.cwd(),
+  request = promisify(require('request')),
+  setTimeout = global.setTimeout,
   commit,
   repository,
   context,
@@ -19,45 +21,57 @@ async function getStatus({
   interval = 1e3,
   timeout = 30e3
 }) {
-  return await new Promise((resolve, reject) => {
-    let start = getTime();
-    let end = start + timeout;
+  if (!commit) {
+    // https://github.com/watson/ci-info/pull/42
+    if (ci.TRAVIS) {
+      if (ci.isPR) {
+        commit = process.env.TRAVIS_PULL_REQUEST_SHA;
+      } else {
+        commit = process.env.TRAVIS_COMMIT;
+      }
+    } else if (process.env.GITHUB_ACTIONS) {
+      // commit = process.env.GITHUB_SHA;
+      commit = (await execa.command('git rev-parse HEAD^2', { cwd })).stdout;
+    } else {
+      throw new Error('CI server not recognized');
+    }
+  }
 
-    (async function getStatus(options = {}) {
+  if (!repository) {
+    // eslint-disable-next-line prefer-let/prefer-let
+    const { pkgUp } = await import('pkg-up');
+
+    repository = require(await pkgUp({ cwd })).repository;
+
+    if (typeof repository !== 'string') {
+      repository = repository.url;
+    }
+  }
+
+  let url = new URL(repository);
+
+  let start = getTime();
+  let end = start + timeout;
+
+  return await new Promise((resolve, reject) => {
+    async function getStatus() {
+      try {
+        let status = await _getStatus(...arguments);
+
+        if (status !== undefined) {
+          resolve(status);
+        }
+      } catch (err) {
+        reject(err);
+      }
+    }
+
+    async function _getStatus(options = {}) {
       let now = getTime();
 
       if (now >= end) {
-        return reject(new Error(`${name} has timed out`));
+        throw new Error(`${name} has timed out`);
       }
-
-      if (!commit) {
-        // https://github.com/watson/ci-info/pull/42
-        if (ci.TRAVIS) {
-          if (ci.isPR) {
-            commit = process.env.TRAVIS_PULL_REQUEST_SHA;
-          } else {
-            commit = process.env.TRAVIS_COMMIT;
-          }
-        } else if (process.env.GITHUB_ACTIONS) {
-          // commit = process.env.GITHUB_SHA;
-          commit = (await execa.command('git rev-parse HEAD^2')).stdout;
-        } else {
-          throw new Error('CI server not recognized');
-        }
-      }
-
-      if (!repository) {
-        // eslint-disable-next-line prefer-let/prefer-let
-        const { pkgUp } = await import('pkg-up');
-
-        repository = require(await pkgUp()).repository;
-      }
-
-      if (typeof repository !== 'string') {
-        repository = repository.url;
-      }
-
-      let url = new URL(repository);
 
       let status;
 
@@ -82,7 +96,7 @@ async function getStatus({
           });
 
           if (response.statusCode === 403) {
-            return reject(new Error(response.body.message));
+            throw new Error(response.body.message);
           }
 
           if (response.statusCode === 304) {
@@ -92,7 +106,7 @@ async function getStatus({
           status = response.body.find(status => status.context === context);
 
           if (status && status.state !== 'pending') {
-            return resolve(status);
+            return status;
           }
 
           options.etag = response.headers.etag;
@@ -100,16 +114,18 @@ async function getStatus({
           break;
         }
         default:
-          throw new Error('git server not recognized');
+          throw new Error(`git server ${url.host} not recognized`);
       }
 
       // https://github.com/watson/ci-info/pull/42
       if (!status && !(ci.isPR || process.env.GITHUB_EVENT_NAME === 'pull_request')) {
-        return resolve(null);
+        return null;
       }
 
       setTimeout(getStatus, interval, options);
-    })();
+    }
+
+    getStatus();
   });
 }
 
